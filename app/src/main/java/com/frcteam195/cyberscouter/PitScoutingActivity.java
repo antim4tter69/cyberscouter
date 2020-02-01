be9a18a288c855ef7aaa8986abc9953885d1f402
Connor Bouffard <cbouff1220@gmail.com>
2020-02-01 15:56:10 -0500

    Attempted to fix tabbed pit scouting page again

--- app/src/main/java/com/frcteam195/cyberscouter/PitScoutingActivity.java
+++ app/src/main/java/com/frcteam195/cyberscouter/PitScoutingActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.Toolbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -21,11 +23,36 @@ public class PitScoutingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pit_scouting);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1 Item"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2 Item"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3 Item"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 4 Item"));
+
+        final PagerAdapter adapter = new TabsPagerAdapter (getSupportFragmentManager(), tabLayout.getTabCount());
+        final ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
         FloatingActionButton fab = findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
@@ -39,21 +66,28 @@ public class PitScoutingActivity extends AppCompatActivity {
 
     public class TabsPagerAdapter extends FragmentPagerAdapter {
 
-        public TabsPagerAdapter(FragmentManager fm) {
+        int tabCount;
+
+        public TabsPagerAdapter(FragmentManager fm, int numberOfTabs) {
             super (fm);
+            this.tabCount = numberOfTabs;
         }
 
         @Override
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return new PhysicalPropertiesTab();
+                    PhysicalPropertiesTab physicalPropertiesTab = new PhysicalPropertiesTab();
+                    return physicalPropertiesTab;
                 case 1:
-                    return new AutoTab();
+                    AutoTab autoTab = new AutoTab();
+                    return autoTab;
                 case 2:
-                    return new TeleopTab();
+                    TeleopTab teleopTab = new TeleopTab();
+                    return teleopTab;
                 case 3:
-                    return new EndgameTab();
+                    EndgameTab endgameTab = new EndgameTab();
+                    return endgameTab;
                 default:
                     return null;
             }
@@ -61,7 +95,7 @@ public class PitScoutingActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 4;
+            return tabCount;
         }
     }
 }

2f122e4ccc44ca55573c45be8dd681e0c9e3b9f9
Connor Bouffard <cbouff1220@gmail.com>
2020-02-03 20:08:37 -0500

    We got TABS now!

--- app/src/main/java/com/frcteam195/cyberscouter/PitScoutingActivity.java
+++ app/src/main/java/com/frcteam195/cyberscouter/PitScoutingActivity.java
@@ -7,8 +7,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v7.widget.Toolbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -23,79 +21,30 @@ public class PitScoutingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pit_scouting);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1 Item"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2 Item"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3 Item"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 4 Item"));
-
-        final PagerAdapter adapter = new TabsPagerAdapter (getSupportFragmentManager(), tabLayout.getTabCount());
-        final ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.setupWithViewPager(viewPager);
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    public class TabsPagerAdapter extends FragmentPagerAdapter {
-
-        int tabCount;
-
-        public TabsPagerAdapter(FragmentManager fm, int numberOfTabs) {
-            super (fm);
-            this.tabCount = numberOfTabs;
-        }
-
-        @Override
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    PhysicalPropertiesTab physicalPropertiesTab = new PhysicalPropertiesTab();
-                    return physicalPropertiesTab;
+                    return new PhysicalPropertiesTab();
                 case 1:
-                    AutoTab autoTab = new AutoTab();
-                    return autoTab;
+                    return new AutoTab();
                 case 2:
-                    TeleopTab teleopTab = new TeleopTab();
-                    return teleopTab;
+                    return new TeleopTab();
                 case 3:
-                    EndgameTab endgameTab = new EndgameTab();
-                    return endgameTab;
+                    return new EndgameTab();
                 default:
                     return null;
             }
         }
 
-        @Override
         public int getCount() {
-            return tabCount;
+            return 4;
         }
-    }
 }

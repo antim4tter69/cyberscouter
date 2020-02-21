efcc7908f2b86543bd6e8827bc85ebc35337a8bd
logan <loganmiceli@icloud.com>
2020-02-20 20:12:47 -0500

    Merge branch 'master' of https://github.com/frcteam195/cyberscouter
    # Conflicts:
    #	app/src/main/java/com/frcteam195/cyberscouter/TelePopUpPage.java
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/frcteam195/cyberscouter/AutoTab.java
+++ app/src/main/java/com/frcteam195/cyberscouter/AutoTab.java
@@ -14,12 +14,17 @@ public class AutoTab extends Fragment {
     private Button button;
     private final int[] moveBonusButtons = {R.id.button_moveBonusNo, R.id.button_moveBonusYes};
     private final int[] pickupButtons = {R.id.button_PickupNo, R.id.button_PickupYes};
+    private View _view;
     private int defaultButtonTextColor;
     private final int SELECTED_BUTTON_TEXT_COLOR = Color.GREEN;
+    private int NumberOfPreloadCount = 0;
+    private int TypicalCellsStoredCount = 0;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_auto, container, false);
+        _view = view;
 
 
 
@@ -78,16 +83,20 @@ public class AutoTab extends Fragment {
 
 
     public void moveBonusNo()  {
-        FakeRadioGroup.buttonPressed(this, 0, moveBonusButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_AUTOMOVEBONUS, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
+        FakeRadioGroup.buttonPressed(getActivity(), _view, 0, moveBonusButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_AUTOMOVEBONUS, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
     }
     public void moveBonusYes()  {
-        FakeRadioGroup.buttonPressed(this, 1, moveBonusButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_AUTOMOVEBONUS, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
+        FakeRadioGroup.buttonPressed(getActivity(), _view, 1, moveBonusButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_AUTOMOVEBONUS, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
     }
 
     public void pickupNo()  {
-        FakeRadioGroup.buttonPressed( this, 0, pickupButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_PICKUP, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
+        FakeRadioGroup.buttonPressed(getActivity(), _view, 0, pickupButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_PICKUP, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
     }
     public void pickupYes()  {
-        getActivity();
+        FakeRadioGroup.buttonPressed(getActivity(), _view, 1, pickupButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_PICKUP, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
+    }
+
+    public void numberOfPreloadMinus() {
+        
     }
 }

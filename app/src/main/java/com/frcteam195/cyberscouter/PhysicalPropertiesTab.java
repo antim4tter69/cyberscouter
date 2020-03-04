84de71bcf77c6f516eb8614b17cb91db3cf36139
bontempi <chrisbontempi@compuserve.com>
2020-03-04 15:24:40 -0500

    Added tab disabling for completed teams.

--- app/src/main/java/com/frcteam195/cyberscouter/PhysicalPropertiesTab.java
+++ app/src/main/java/com/frcteam195/cyberscouter/PhysicalPropertiesTab.java
@@ -1,14 +1,11 @@
 package com.frcteam195.cyberscouter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;

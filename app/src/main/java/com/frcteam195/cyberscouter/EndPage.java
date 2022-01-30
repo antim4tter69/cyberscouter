1e198951802dc19037f19cf6e7744e99714668de
LoganAM <46632887+LoganAM@users.noreply.github.com>
2022-01-30 16:21:02 -0500

    Worked on endgame

--- app/src/main/java/com/frcteam195/cyberscouter/EndPage.java
+++ app/src/main/java/com/frcteam195/cyberscouter/EndPage.java
@@ -14,16 +14,11 @@ public class EndPage extends AppCompatActivity {
     private Button button;
     private int defaultButtonTextColor = Color.LTGRAY;
     private final int SELECTED_BUTTON_TEXT_COLOR = Color.GREEN;
-    private final int[] lostCommButtons = {R.id.button_LostCommNo, R.id.button_LostCommYes};
-    private final int[] brokeDownButtons = {R.id.button_BrokeDownNo, R.id.button_BrokeDownYes};
-    private final int[] subSystemBrokeButtons = {R.id.button_SubSystemBrokeNo, R.id.button_SubSystemBrokeYes};
-    private final int[] groundPickupButtons = {R.id.button_GroundPickupNo, R.id.button_GroundPickupYes};
-    private final int[] hopperLoadButtons = {R.id.button_HopperLoadNo, R.id.button_HopperLoadYes};
     private final int[] defenseButtons = {R.id.button_DefenseNone, R.id.button_DefenseWeak, R.id.button_DefenseStrong};
     private final int[] levelButtons = {R.id.button_LevelNo, R.id.button_LevelYes};
     private final int[] movedOnBarButtons = {R.id.button_MovedOnBarNo, R.id.button_MovedOnBarYes};
     private final int[] playedDefenseButtons = {R.id.button_PlayedDefenseNo, R.id.button_PlayedDefenseYes};
-    private final int[] climbStatusButtons = {R.id.button_noAttemptClimb,R.id.button_Climbed, R.id.button_AttemptedClimb, R.id.button_BusyClimb, R.id.button_ParkedClimb};
+    private final int[] climbStatusButtons = {R.id.button_noAttemptClimb,R.id.button_Traversal, R.id.button_High, R.id.button_Middle, R.id.button_Low};
 
     String[] _lColumns = {CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMLOSTCOMM,
             CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMBROKEDOWN,
@@ -72,86 +67,6 @@ public class EndPage extends AppCompatActivity {
             }
         });
 
-        button = findViewById(R.id.button_LostCommNo);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                lostCommNo();
-
-            }
-        });
-        button = findViewById(R.id.button_LostCommYes);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                lostCommYes();
-
-            }
-        });
-        button = findViewById(R.id.button_BrokeDownNo);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                brokeDownNo();
-
-            }
-        });
-        button = findViewById(R.id.button_BrokeDownYes);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                brokeDownYes();
-
-            }
-        });
-        button = findViewById(R.id.button_SubSystemBrokeNo);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                subSystemBrokeNo();
-
-            }
-        });
-        button = findViewById(R.id.button_SubSystemBrokeYes);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                subSystemBrokeYes();
-
-            }
-        });
-        button = findViewById(R.id.button_GroundPickupNo);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                groundPickupNo();
-
-            }
-        });
-        button = findViewById(R.id.button_GroundPickupYes);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                groundPickupYes();
-
-            }
-        });
-        button = findViewById(R.id.button_HopperLoadNo);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hopperLoadNo();
-
-            }
-        });
-        button = findViewById(R.id.button_HopperLoadYes);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                hopperLoadYes();
-
-            }
-        });
         button = findViewById(R.id.button_DefenseNone);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -230,7 +145,7 @@ public class EndPage extends AppCompatActivity {
                 noAttemptClimb();
             }
         });
-        button = findViewById(R.id.button_Climbed);
+        button = findViewById(R.id.button_Traversal);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -238,7 +153,7 @@ public class EndPage extends AppCompatActivity {
 
             }
         });
-        button = findViewById(R.id.button_AttemptedClimb);
+        button = findViewById(R.id.button_High);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -246,7 +161,7 @@ public class EndPage extends AppCompatActivity {
 
             }
         });
-        button = findViewById(R.id.button_BusyClimb);
+        button = findViewById(R.id.button_Middle);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -254,7 +169,7 @@ public class EndPage extends AppCompatActivity {
 
             }
         });
-        button = findViewById(R.id.button_ParkedClimb);
+        button = findViewById(R.id.button_Low);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -278,19 +193,9 @@ public class EndPage extends AppCompatActivity {
         if (null != csm) {
             TextView tv = findViewById(R.id.textView_endgameMatch);
             tv.setText(getString(R.string.tagMatch, csm.getMatchNo()));
-            tv = findViewById(R.id.textView6_endgameTeam);
+            tv = findViewById(R.id.textView_endgameTeam);
             tv.setText(getString(R.string.tagTeam, csm.getTeam()));
 
-            FakeRadioGroup.buttonDisplay(this, csm.getSummLostComm(), lostCommButtons, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-            lostComm = csm.getSummLostComm();
-            FakeRadioGroup.buttonDisplay(this, csm.getSummBrokeDown(), brokeDownButtons, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-            brokeDown = csm.getSummBrokeDown();
-            FakeRadioGroup.buttonDisplay(this, csm.getSummSubsystemBroke(), subSystemBrokeButtons, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-            subSystemBroke = csm.getSummSubsystemBroke();
-            FakeRadioGroup.buttonDisplay(this, csm.getSummGroundPickup(), groundPickupButtons, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-            groudPickup = csm.getSummGroundPickup();
-            FakeRadioGroup.buttonDisplay(this, csm.getSummHopperLoad(), hopperLoadButtons, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-            hopperLoad = csm.getSummHopperLoad();
             FakeRadioGroup.buttonDisplay(this, csm.getSummDefPlayedAgainst(), defenseButtons, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
             defense = csm.getSummDefPlayedAgainst();
             FakeRadioGroup.buttonDisplay(this, csm.getClimbLevelStatus(), levelButtons, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
@@ -317,46 +222,6 @@ public class EndPage extends AppCompatActivity {
 
     }
 
-    public void lostCommNo() {
-        FakeRadioGroup.buttonPressed(this, 0, lostCommButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMLOSTCOMM, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        lostComm = 0;
-    }
-    public void lostCommYes() {
-        FakeRadioGroup.buttonPressed(this, 1, lostCommButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMLOSTCOMM, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        lostComm = 1;
-    }
-    public void brokeDownNo() {
-        FakeRadioGroup.buttonPressed(this, 0, brokeDownButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMBROKEDOWN, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        brokeDown = 0;
-    }
-    public void brokeDownYes() {
-        FakeRadioGroup.buttonPressed(this, 1, brokeDownButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMBROKEDOWN, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        brokeDown = 1;
-    }
-    public void subSystemBrokeNo() {
-        FakeRadioGroup.buttonPressed(this, 0, subSystemBrokeButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMSUBSYSTEMBROKE, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        subSystemBroke = 0;
-    }
-    public void subSystemBrokeYes() {
-        FakeRadioGroup.buttonPressed(this, 1, subSystemBrokeButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMSUBSYSTEMBROKE, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        subSystemBroke = 1;
-    }
-    public void groundPickupNo() {
-        FakeRadioGroup.buttonPressed(this, 0, groundPickupButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMGROUNDPICKUP, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        groudPickup = 0;
-    }
-    public void groundPickupYes() {
-        FakeRadioGroup.buttonPressed(this, 1, groundPickupButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMGROUNDPICKUP, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        groudPickup = 1;
-    }
-    public void hopperLoadNo() {
-        FakeRadioGroup.buttonPressed(this, 0, hopperLoadButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMHOPPERLOAD, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        hopperLoad = 0;
-    }
-    public void hopperLoadYes() {
-        FakeRadioGroup.buttonPressed(this, 1, hopperLoadButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMHOPPERLOAD, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
-        hopperLoad = 1;
-    }
     public void defenseNone() {
         FakeRadioGroup.buttonPressed(this, 0, defenseButtons, CyberScouterContract.MatchScouting.COLUMN_NAME_SUMMDEFPLAYEDAGAINST, SELECTED_BUTTON_TEXT_COLOR, defaultButtonTextColor);
         defense = 0;

63e341d51825d45c09701d127ec671724b649faf
LoganAM <46632887+LoganAM@users.noreply.github.com>
2022-02-14 20:30:44 -0500

    Worked on Scouting 2 page 2, need assistance with dropdown menus most likely

--- app/src/main/java/com/frcteam195/cyberscouter/LevelTwoScoutingTwo.java
+++ app/src/main/java/com/frcteam195/cyberscouter/LevelTwoScoutingTwo.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LevelTwoScoutingTwo extends AppCompatActivity {
+    private final int[] MultiRobotDefenseButtons = {R.id.button_MRDYes, R.id.button_MRDNo};
+
+    //String[] _lColumns = {CyberScouterContract.MatchScouting.COLUMN_NAME_MULTIROBOTDEFENSE};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
